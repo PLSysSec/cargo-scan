dbb6fe9f3e8fb9dcb13072a4e61ec3eb6c35494e
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-09-23 12:49:18 -0700

    :wrench: implement scan_crate

--- src/gather.py
+++ src/gather.py
@@ -2,12 +2,34 @@
 Script to download Cargo crate source code and analyze module-level imports.
 """
 
+import os
+import re
 import subprocess
 
 PACKAGES_DIR = "packages"
+SRC_DIR = "src"
 
 def download_crate(name):
-    subprocess.run(["cargo", "download", "-x", name, "-o", f"{PACKAGES_DIR}/{name}"])
+    target = os.path.join(PACKAGES_DIR, name)
+    subprocess.run(["cargo", "download", "-x", name, "-o", target])
 
-download_crate("rand")
-download_crate("syn")
+def scan_file(root, file):
+    filepath = os.path.join(root, file)
+    print(f"scanning file: {filepath}")
+    with open(filepath) as fh:
+        for line in fh:
+            if re.match("use .*", line):
+                print(root, line)
+
+def scan_crate(name):
+    print(f"scanning crate: {name}")
+    src = os.path.join(PACKAGES_DIR, name, SRC_DIR)
+    for root, dirs, files in os.walk(src):
+        for file in files:
+            if os.path.splitext(file)[1] == ".rs":
+                scan_file(root, file)
+
+# download_crate("rand")
+# download_crate("syn")
+scan_crate("rand")
+# scan_rate("syn")
