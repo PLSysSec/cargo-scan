d83c7184d07e7e7ce96c3602dcc81b2b5597889f
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-09-23 20:16:16 -0700

    :wrench: use logging

--- src/gather.py
+++ src/gather.py
@@ -2,6 +2,7 @@
 Script to download Cargo crate source code and analyze module-level imports.
 """
 
+import logging
 import os
 import re
 import subprocess
@@ -9,29 +10,32 @@ import time
 
 PACKAGES_DIR = "packages"
 SRC_DIR = "src"
-
 RESULTS_DIR = "results"
+
+logging.basicConfig(level=logging.INFO)
+
+# Main script
 
 def download_crate(crate):
     target = os.path.join(PACKAGES_DIR, crate)
     if os.path.exists(target):
-        print(f"found existing crate: {target}")
+        logging.info(f"found existing crate: {target}")
     else:
-        print(f"downloading crate: {target}")
+        logging.info(f"downloading crate: {target}")
         subprocess.run(["cargo", "download", "-x", crate, "-o", target])
 
 def save_results(crate, results):
     timestr = time.strftime("%Y%m%d_%H%M%S")
     results_file = f"{timestr}_{crate}.csv"
     results_path = os.path.join(RESULTS_DIR, results_file)
-    print(f"Saving results to {results_path}")
+    logging.info(f"Saving results to {results_path}")
     with open(results_path, 'a') as fh:
         for line in results:
             fh.write(line)
 
 def scan_file(crate, root, file, results):
     filepath = os.path.join(root, file)
-    print(f"scanning file: {filepath}")
+    logging.debug(f"scanning file: {filepath}")
     with open(filepath) as fh:
         for line in fh:
             if re.match("use .*$", line):
@@ -39,7 +43,7 @@ def scan_file(crate, root, file, results):
 
 def scan_crate(crate):
     results = []
-    print(f"scanning crate: {crate}")
+    logging.info(f"scanning crate: {crate}")
     src = os.path.join(PACKAGES_DIR, crate, SRC_DIR)
     for root, dirs, files in os.walk(src):
         for file in files:
