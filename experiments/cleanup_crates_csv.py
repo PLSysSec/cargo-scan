be30be02b87694076afc9c231db53c45fba560b2
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-09-27 16:44:53 -0700

    :sparkles: readability + typo

--- experiments/cleanup_crates_csv.py
+++ experiments/cleanup_crates_csv.py
@@ -33,16 +33,39 @@ with open(CRATES_CSV, newline='') as infile:
         # 9. repository
         # 10. updated_at
 
-        # Cleanup:
+        # Ignored rows:
         # Index 8 is the offending row (a README dump) causing most
         # of the bloat
         # Index 6 doesn't seem to contain any data
-        # Index 1 sometimes contains newlines
-        del row[8]
-        del row[6]
-        row[1] = row[1].replace('\n', ' ')
+        created_at = row[0]
+        description = row[1]
+        documentation = row[2]
+        downloads = row[3]
+        homepage = row[4]
+        id = row[5]
+        name = row[7]
+        repository = row[9]
+        updated_at = row[10]
 
-        rows.append(row)
+        # Cleanup:
+        # description sometimes contains newlines. And is sometimes too long
+        if '\n' in description:
+            description = description.replace('\n', ' ')
+            # print(f"Removed newlines: {description}")
+
+        out_row = [
+            created_at,
+            description,
+            documentation,
+            downloads,
+            homepage,
+            id,
+            name,
+            repository,
+            updated_at,
+        ]
+
+        rows.append(out_row)
 
 # Sort data by downloads
 first_row = rows[0]
@@ -54,7 +77,7 @@ with open(OUT_CSV, 'w', newline='') as outfile:
     out_writer = csv.writer(outfile, delimiter=',', lineterminator='\n')
     for row in rows:
         out_writer.writerow(row)
-    print(f"Successfully rote {len(rows)} rows")
+    print(f"Successfully wrote {len(rows)} rows")
 
 rows = rows[:1001]
 
@@ -62,4 +85,4 @@ with open(OUT_CSV_FIRST1000, 'w', newline='') as outfile:
     out_writer = csv.writer(outfile, delimiter=',', lineterminator='\n')
     for row in rows:
         out_writer.writerow(row)
-    print(f"Successfully rote {len(rows)} rows")
+    print(f"Successfully wrote {len(rows)} rows")
