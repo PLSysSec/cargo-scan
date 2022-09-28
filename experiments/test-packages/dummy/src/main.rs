f8a7c052a9a86ba9beee96e0673539b85c91b3fd
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-09-28 16:50:35 -0700

    :test_tube: expand parsing test cases

--- experiments/test-packages/dummy/src/main.rs
+++ experiments/test-packages/dummy/src/main.rs
@@ -3,17 +3,28 @@ use serde::{Serialize, Deserialize};
 use std::fs::*;
 use std::os::*;
 
-// nested and multi-line braces
+// nested braces
 use std::fs::{mod1, mod2::{test1, test2}};
+use std::{a::b::{c::d, e::f}, process::{process::Command, Command}::*};
+
+// multi-line braces
 use std::fs::test::{
-    mod1,
-    mod2
+    mod3,
+    mod4
+};
+use std::{
+    a, fs::mod5
+};
+use std::{
+    env,
+    b,
 }
+;
 
 // comments
-use std::fs::mod3; // comment, with, commas, in, it
+use std::fs::mod6; // comment, with, commas, in, it
 use std::collections::*; // comment with import: std::fs
-use std::fs::mod4;//comment-without-spaces
+use std::fs::mod7;//comment-without-spaces
 
 // example missed in rustc_version
 use std::{env, error, fmt, io, num, str};
