4c11d8d2c2212fd4cbb89992ee6993ab8f861f1e
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-09-28 14:19:45 -0700

    :test_tube: add a parsing failure

--- experiments/test-packages/dummy/src/main.rs
+++ experiments/test-packages/dummy/src/main.rs
@@ -2,11 +2,19 @@
 use serde::{Serialize, Deserialize};
 use std::fs::*;
 use std::os::*;
+
+// nested and multi-line braces
 use std::fs::{mod1, mod2::{test1, test2}};
 use std::fs::test::{
     mod1,
     mod2
 }
+
+// comments
 use std::fs::mod3; // comment, with, commas, in, it
 use std::collections::*; // comment with import: std::fs
 use std::fs::mod4;//comment-without-spaces
+
+// example missed in rustc_version
+use std::{env, error, fmt, io, num, str};
+use std::{error, env}; // a variant
