fffec138b2adc5ab16f048982df18d3f295195ad
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-11-03 00:31:55 -0700

    :wrench: toy version of a url client

--- data/test-packages/toy-crates/src/url_client.rs
+++ data/test-packages/toy-crates/src/url_client.rs
@@ -1,1 +1,17 @@
-// TODO
+/*
+    Toy example of a Url client
+    (Note: this code doesn't work for many reasons)
+*/
+
+use super::url::Url;
+use std::net::TcpListener;
+
+pub fn ping_example_com() {
+    let url = Url::parse("https://www.example.com").unwrap();
+    // I know this isn't valid, we need to use ToSocketAddrs etc. to
+    // get a socket, just doing this as an example
+    let listener = TcpListener::bind(url.path).unwrap();
+    for stream in listener.incoming() {
+        println!("{:?}", stream.unwrap());
+    }
+}
