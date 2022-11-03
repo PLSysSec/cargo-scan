4b8c36a42b4f4e11791528d13c467de7cd045c3d
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-11-02 23:23:19 -0700

    :wrench: toy version of rustc_version

--- data/test-packages/toy-crates/src/rustc_version.rs
+++ data/test-packages/toy-crates/src/rustc_version.rs
@@ -1,1 +1,18 @@
-// TODO
+/*
+    Toy version of the rustc_version crate.
+*/
+
+use std::env;
+use std::ffi::OsString;
+use std::process::Command;
+
+// VersionMeta::for_command
+pub fn version_meta_for_command(mut cmd: Command) -> String {
+    let out = cmd.arg("-vV").output().unwrap();
+    String::from_utf8(out.stdout).unwrap()
+}
+
+pub fn version_meta() -> String {
+    let cmd = env::var_os("RUSTC").unwrap_or_else(|| OsString::from("rustc"));
+    version_meta_for_command(Command::new(cmd))
+}
