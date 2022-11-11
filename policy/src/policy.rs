c32869b240719566cac9b7010dae5dd88e1d992f
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-11-10 22:01:38 -0800

    :wrench: work in progress: fixing serialization

--- policy/src/policy.rs
+++ policy/src/policy.rs
@@ -5,16 +5,16 @@
     See example .policy files in policies/
 */
 
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::{self, Display};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct Expr(String);
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct Args(String);
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "c")]
+#[derive(Debug)]
 pub enum Effect {
     EnvRead(Expr),
     EnvWrite(Expr),
@@ -24,6 +24,22 @@ pub enum Effect {
     // NetRecv(String),
     // NetSend(String),
     Exec(Vec<Expr>),
+}
+impl Display for Effect {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::EnvRead(e) => write!(f, "env_read({})", e.0),
+            Self::EnvWrite(e) => write!(f, "env_write({})", e.0),
+            Self::FsRead(e) => write!(f, "fs_read({})", e.0),
+            Self::FsWrite(e) => write!(f, "fs_write({})", e.0),
+            Self::Exec(v) => {
+                // Ineffecient implementation bc .intersperse() / .join()
+                // on iterators is not stable
+                let v: Vec<&str> = v.iter().map(|e| e.0.as_ref()).collect();
+                write!(f, "exec({})", v.join(", "))
+            }
+        }
+    }
 }
 impl Effect {
     pub fn env_read(s: &str) -> Self {
@@ -59,54 +75,121 @@ impl Effect {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "c")]
+#[derive(Debug)]
 pub enum Region {
     Crate(String),
     Module(String),
     Function(String, Args),
     FunctionAll(String),
 }
+impl Display for Region {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Crate(s) => write!(f, "{}", s),
+            Self::Module(s) => write!(f, "::{}", s),
+            Self::Function(s, a) => write!(f, "::{}({})", s, a.0),
+            Self::FunctionAll(s) => write!(f, "::{}()", s),
+        }
+    }
+}
 
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(tag = "type", content = "c")]
+#[derive(Debug)]
 pub enum Statement {
-    Allow(Region, Effect),
-    Require(Region, Effect),
-    Trust(Region),
+    Allow { region: Region, effect: Effect },
+    Require { region: Region, effect: Effect },
+    Trust { region: Region },
+}
+impl Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Allow { region, effect } => {
+                write!(f, "allow {} {}", region, effect)
+            }
+            Self::Require { region, effect } => {
+                write!(f, "require {} {}", region, effect)
+            }
+            Self::Trust { region } => {
+                write!(f, "trust {}", region)
+            }
+        }
+    }
+}
+/// Serialization for the Statement type
+///
+/// Serde is unfortunately rather clunky at serializing enums
+/// with unnamed fields in each enum variant.
+/// E.g., it generates things like
+/// ```
+/// [[statements]]
+/// type = "Allow"
+///
+/// [statements.expr]
+/// region = ["prepare_data", ""]
+///
+/// [statements.expr.effect]
+/// type = "FsWrite"
+/// expr = "my_app.log"
+/// ```
+///
+/// Here we leverage Display to get a more compact representation:
+/// ```
+/// allow prepare_data() FsWrite("my_app.log")
+/// ```
+impl Serialize for Statement {
+    fn serialize<S>(&self, ser: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        ser.collect_str(self)
+    }
+}
+impl<'de> Deserialize<'de> for Statement {
+    fn deserialize<D>(_deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        todo!()
+    }
 }
 impl Statement {
-    pub fn allow_crate(name: &str, eff: Effect) -> Self {
-        Self::Allow(Region::Crate(name.to_string()), eff)
+    pub fn allow_crate(name: &str, effect: Effect) -> Self {
+        let region = Region::Crate(name.to_string());
+        Self::Allow { region, effect }
     }
-    pub fn allow_mod(name: &str, eff: Effect) -> Self {
-        Self::Allow(Region::Module(name.to_string()), eff)
+    pub fn allow_mod(name: &str, effect: Effect) -> Self {
+        let region = Region::Module(name.to_string());
+        Self::Allow { region, effect }
     }
-    pub fn allow_fn(name: &str, args: &str, eff: Effect) -> Self {
+    pub fn allow_fn(name: &str, args: &str, effect: Effect) -> Self {
         let name = name.to_string();
         let args = args.to_string();
-        Self::Allow(Region::Function(name, Args(args)), eff)
+        let region = Region::Function(name, Args(args));
+        Self::Allow { region, effect }
     }
     // Q: do we need these?
-    // pub fn require_crate(name: &str, eff: Effect) -> Self {
-    //     Self::Require(Region::Crate(name.to_string()), eff)
+    // pub fn require_crate(name: &str, effect: Effect) -> Self {
+    //     Self::Require(Region::Crate(name.to_string()), effect)
     // }
-    // pub fn require_mod(name: &str, eff: Effect) -> Self {
-    //     Self::Require(Region::Module(name.to_string()), eff)
+    // pub fn require_mod(name: &str, effect: Effect) -> Self {
+    //     Self::Require(Region::Module(name.to_string()), effect)
     // }
-    pub fn require_fn(name: &str, args: &str, eff: Effect) -> Self {
+    pub fn require_fn(name: &str, args: &str, effect: Effect) -> Self {
         let name = name.to_string();
         let args = args.to_string();
-        Self::Require(Region::Function(name, Args(args)), eff)
+        let region = Region::Function(name, Args(args));
+        Self::Require { region, effect }
     }
     pub fn trust_crate(name: &str) -> Self {
-        Self::Trust(Region::Crate(name.to_string()))
+        let region = Region::Crate(name.to_string());
+        Self::Trust { region }
     }
     pub fn trust_mod(name: &str) -> Self {
-        Self::Trust(Region::Module(name.to_string()))
+        let region = Region::Module(name.to_string());
+        Self::Trust { region }
     }
     pub fn trust_fn(name: &str) -> Self {
-        Self::Trust(Region::FunctionAll(name.to_string()))
+        let region = Region::FunctionAll(name.to_string());
+        Self::Trust { region }
     }
 }
 
