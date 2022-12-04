36a87c3d1d0cb5c727b06c3b3d8c0c9bbea96b16
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-12-03 22:57:38 -0800

    Scanner::scan_mod

--- rust-src/src/bin/find_calls.rs
+++ rust-src/src/bin/find_calls.rs
@@ -15,41 +15,52 @@ struct Args {
 }
 
 /// Stateful object to scan Rust source code for effects (fn calls of interest)
-struct Scanner {
+struct Scanner<'a> {
     results: Vec<Effect>,
+    scope_mods: Vec<&'a syn::Ident>,
 }
 
-impl Scanner {
+impl<'a> Scanner<'a> {
     fn new() -> Self {
         let results = Vec::new();
-        Self { results }
+        let scope_mods = Vec::new();
+        Self { results, scope_mods }
     }
-    fn scan_use(&mut self, _u: &syn::ItemUse) {
-        todo!()
-    }
-    fn scan_mod(&mut self, _m: &syn::ItemMod) {
-        todo!()
-    }
-    fn scan_fn(&mut self, _f: &syn::ItemFn) {
-        todo!()
-    }
-    fn scan_file(&mut self, f: &syn::File) {
+    fn scan_file(&mut self, f: &'a syn::File) {
         // scan the file and return a list of all calls in it
-        for item in &f.items {
-            match item {
-                syn::Item::Use(u) => self.scan_use(u),
-                syn::Item::Mod(m) => self.scan_mod(m),
-                syn::Item::Fn(fun) => self.scan_fn(fun),
-                _ => (),
-                // For all syntax elements see
-                // https://docs.rs/syn/latest/syn/enum.Item.html
-                // Potentially interesting:
-                // Trait(t) -- default impls are an issue here
-                // ForeignMod(ItemForeignMod) -- extern items like extern "C" { ... }
-                // Macro, Macro2 -- macro invocations and declarations
-                // Const(ItemConst), Static(ItemStatic) -- for information flow
-            }
+        for i in &f.items {
+            self.scan_item(i);
         }
+    }
+    fn scan_item(&mut self, i: &'a syn::Item) {
+        match i {
+            syn::Item::Mod(m) => self.scan_mod(m),
+            syn::Item::Use(u) => self.scan_use(u),
+            syn::Item::Fn(fun) => self.scan_fn(fun),
+            _ => (),
+            // For all syntax elements see
+            // https://docs.rs/syn/latest/syn/enum.Item.html
+            // Potentially interesting:
+            // Trait(t) -- default impls are an issue here
+            // ForeignMod(ItemForeignMod) -- extern items like extern "C" { ... }
+            // Macro, Macro2 -- macro invocations and declarations
+            // Const(ItemConst), Static(ItemStatic) -- for information flow
+        }
+    }
+    fn scan_mod(&mut self, m: &'a syn::ItemMod) {
+        if let Some((_, items)) = &m.content {
+            self.scope_mods.push(&m.ident);
+            for i in items {
+                self.scan_item(i);
+            }
+            self.scope_mods.pop();
+        }
+    }
+    fn scan_use(&mut self, _u: &'a syn::ItemUse) {
+        todo!()
+    }
+    fn scan_fn(&mut self, _f: &'a syn::ItemFn) {
+        todo!()
     }
 }
 
