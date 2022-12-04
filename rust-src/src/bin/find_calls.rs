e6957e6495902f2a9f0ec27fc245096e44766fa2
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-12-03 22:02:37 -0800

    skeleton Scanner object

--- rust-src/src/bin/find_calls.rs
+++ rust-src/src/bin/find_calls.rs
@@ -14,6 +14,45 @@ struct Args {
     filename: String,
 }
 
+/// Stateful object to scan Rust source code for effects (fn calls of interest)
+struct Scanner {
+    results: Vec<Effect>,
+}
+
+impl Scanner {
+    fn new() -> Self {
+        let results = Vec::new();
+        Self { results }
+    }
+    fn scan_use(&mut self, _u: &syn::ItemUse) {
+        todo!()
+    }
+    fn scan_mod(&mut self, _m: &syn::ItemMod) {
+        todo!()
+    }
+    fn scan_fn(&mut self, _f: &syn::ItemFn) {
+        todo!()
+    }
+    fn scan_file(&mut self, f: &syn::File) {
+        // scan the file and return a list of all calls in it
+        for item in &f.items {
+            match item {
+                syn::Item::Use(u) => self.scan_use(u),
+                syn::Item::Mod(m) => self.scan_mod(m),
+                syn::Item::Fn(fun) => self.scan_fn(fun),
+                _ => (),
+                // For all syntax elements see
+                // https://docs.rs/syn/latest/syn/enum.Item.html
+                // Potentially interesting:
+                // Trait(t) -- default impls are an issue here
+                // ForeignMod(ItemForeignMod) -- extern items like extern "C" { ... }
+                // Macro, Macro2 -- macro invocations and declarations
+                // Const(ItemConst), Static(ItemStatic) -- for information flow
+            }
+        }
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
@@ -21,6 +60,12 @@ fn main() {
     let mut file = File::open(&args.filename).expect("Unable to open file");
     let mut src = String::new();
     file.read_to_string(&mut src).expect("Unable to read file");
-    let syntax = syn::parse_file(&src).expect("Unable to parse file");
-    println!("{:#?}", syntax);
+
+    let syntax_tree = syn::parse_file(&src).expect("Unable to parse file");
+    let mut scanner = Scanner::new();
+    scanner.scan_file(&syntax_tree);
+
+    for effect in scanner.results {
+        println!("{}", effect.to_csv());
+    }
 }
