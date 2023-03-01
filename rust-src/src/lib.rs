93fcbb2674e9ba53479824c41cf141e5728b5eb1
Caleb Stanford <caleb.pirsquared@gmail.com>
2023-03-01 11:40:28 -0800

    name resolution placeholder

--- rust-src/src/lib.rs
+++ rust-src/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod effect;
 pub mod ident;
+pub mod name_resolution;
 pub mod policy;
 pub mod scanner;
 pub mod sink;
---
+++ rust-src/src/name_resolution.rs
@@ -0,0 +1,24 @@
+use super::ident;
+
+/// Path that is fully expanded and canonical
+/// e.g. if I do `use libc::foobar as baz`
+/// then `baz` is a valid Path, but not a valid
+/// CanonicalPath
+///
+/// NB: `libc` and `foobar` are valid Idents and valid Paths
+/// `libc::foobar` is a Path but not an Ident
+pub struct CanonicalPath(Path);
+
+/// SrcLoc: file, line, col
+/// Ident: method call or function call
+/// e.g. push
+
+/// Return canonical path which that ident resolves to
+/// e.g. for push we should return `std::vec::Vec::push`
+pub fn resolve_ident(s: SrcLoc, i: Ident) -> CanonicalPath {
+    todo!()
+}
+
+pub fn resolve_path(s: SrcLoc, p: Path) -> CanonicalPath {
+    todo!()
+}
