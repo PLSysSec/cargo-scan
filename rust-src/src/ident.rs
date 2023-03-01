ca55530aa9bc00fe91a9fe115062ee9ce557f02c
David Thien <dthien@eng.ucsd.edu>
2023-02-28 20:15:49 -0800

    Fixed bug not checking including colons for path names in the allowed characters for identifiers

--- rust-src/src/ident.rs
+++ rust-src/src/ident.rs
@@ -27,9 +27,10 @@ impl Display for Ident {
         self.0.fmt(f)
     }
 }
+
 impl Ident {
     fn char_ok(c: char) -> bool {
-        c.is_ascii_alphanumeric() || c == '_' || c == '[' || c == ']'
+        c.is_ascii_alphanumeric() || c == '_' || c == '[' || c == ']' || c == ':'
     }
     pub fn invariant(&self) -> bool {
         self.0.chars().all(Self::char_ok)
@@ -59,6 +60,7 @@ impl Display for Path {
         self.0.fmt(f)
     }
 }
+
 impl Path {
     fn char_ok(c: char) -> bool {
         c.is_ascii_alphanumeric() || c == '_' || c == '[' || c == ']' || c == ':'
