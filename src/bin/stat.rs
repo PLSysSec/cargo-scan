3366f66c386a6047fc85d1a0a43b3343fa19c8d0
David Thien <dthien@eng.ucsd.edu>
2023-08-10 14:40:59 -0700

    Add download option for root crate in stat binary

--- src/bin/stat.rs
+++ src/bin/stat.rs
@@ -1,7 +1,9 @@
+use std::fs::create_dir_all;
 use std::{fs::remove_dir_all, path::PathBuf};
 
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use cargo_scan::auditing::info::Config;
+use cargo_scan::download_crate;
 use cargo_scan::{
     audit_chain::{create_new_audit_chain, Create},
     auditing::review::review_policy,
@@ -10,18 +12,28 @@ use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug, Clone)]
 struct Args {
-    /// Path or name of the crate to audit
+    /// Path or name of the crate to audit. If downloading a crate, it will be saved here.
     crate_path: String,
 
     #[clap(short = 't', long = "crate-path-type", default_value_t = PathType::Local)]
     path_type: PathType,
 
     /// Path to download crates to for auditing
-    #[clap(short = 'd', long = "crate-download-path", default_value = ".stats_tmp")]
-    crate_download_path: String,
+    #[clap(short = 'd', long, default_value = ".stats_policies_tmp")]
+    policy_file_path: String,
+
+    /// Download the crate and save it to the crate_path instead of using an
+    /// existing crate. If this value is set, requires `download_version` to be
+    /// set as well.
+    #[clap(short = 'd', long, requires = "download_version")]
+    pub download_root_crate: Option<String>,
+
+    /// The crate version to be downloaded. Should be used alongside
+    /// `download_root_crate`.
+    #[clap(short = 'v', long)]
+    pub download_version: Option<String>,
 }
 
-// TODO: Add non crates.io remote
 #[derive(ValueEnum, Clone, Copy, Debug)]
 enum PathType {
     Local,
@@ -38,21 +50,58 @@ impl std::fmt::Display for PathType {
     }
 }
 
+// TODO: Figure out who is responsible for clearing the policy path so we don't
+//       re-use audited policies.
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    // TODO: Download path if applicable
+    if let (Some(crate_name), Some(crate_version)) =
+        (&args.download_root_crate, &args.download_version)
+    {
+        let crate_path = PathBuf::from(args.crate_path.clone());
+        if crate_path.exists() {
+            return Err(anyhow!(
+                "Something already exists at the root crate path: {}",
+                args.crate_path
+            ));
+        }
+
+        create_dir_all(crate_path)?;
+        let downloaded_path = download_crate::download_crate_from_info(
+            crate_name,
+            crate_version,
+            &args.crate_path,
+        )?;
+
+        // We have now downloaded the crate into a subfolder of the
+        // crate_path, so we should move it up where the user expects it
+        let mut tmp_path = PathBuf::from(&args.crate_path);
+        let bottom_folder = tmp_path
+            .file_name()
+            .context("No bottom folder in user crate path")?
+            .to_os_string();
+        let bottom_folder_str = bottom_folder.to_string_lossy();
+        tmp_path.pop();
+        tmp_path.push(format!("{}-tmp", bottom_folder_str));
+        std::fs::rename(&downloaded_path, &tmp_path)?;
+
+        let mut parent_downloaded_path = PathBuf::from(&downloaded_path);
+        parent_downloaded_path.pop();
+        std::fs::remove_dir(&parent_downloaded_path)?;
+
+        std::fs::rename(&tmp_path, &args.crate_path)?;
+    }
 
     let create = Create::new(
         args.crate_path.clone(),
-        format!("{}/crate.manifest", &args.crate_download_path),
-        args.crate_download_path.clone(),
+        format!("{}/crate.manifest", &args.policy_file_path),
+        args.policy_file_path.clone(),
         false,
         None,
         None,
     );
 
-    let mut chain = create_new_audit_chain(create, &args.crate_download_path)?;
+    let mut chain = create_new_audit_chain(create, &args.policy_file_path)?;
     let root_crate = chain.root_crate()?;
     let root_policy = chain
         .read_policy(&root_crate)?
@@ -60,7 +109,7 @@ fn main() -> Result<()> {
     let review_config = Config::new(0, 0);
     review_policy(&root_policy, &PathBuf::from(&args.crate_path), &review_config)?;
 
-    remove_dir_all(&args.crate_download_path)?;
+    remove_dir_all(&args.policy_file_path)?;
 
     Ok(())
 }
