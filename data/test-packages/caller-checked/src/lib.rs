e1bbd4304cc4dbc2114936632c18492fa0bd402f
Caleb Stanford <caleb.pirsquared@gmail.com>
2023-10-25 19:43:34 -0700

    :test_tube: expand test case

--- data/test-packages/caller-checked/src/lib.rs
+++ data/test-packages/caller-checked/src/lib.rs
@@ -4,7 +4,13 @@ pub fn no_effect() {
     println!("no_effect");
 }
 
-pub fn has_effect() {
+pub fn has_direct_effect() {
+    unsafe {
+        libc::sysconf(57);
+    }
+}
+
+pub fn has_indirect_effect() {
     println!("has_effect");
     sub::effect();
 }
